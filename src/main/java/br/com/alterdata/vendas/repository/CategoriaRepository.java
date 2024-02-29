731bc0583ee663c8f249aa5cbca8488d5beef8f5
diegors <diegors@cpqd.com.br>
2024-02-28 22:31:48 -0300

    Classes de persistência de Produto e Categoria

--- src/main/java/br/com/alterdata/vendas/repository/CategoriaRepository.java
+++ src/main/java/br/com/alterdata/vendas/repository/CategoriaRepository.java
@@ -1,13 +1,10 @@
 package br.com.alterdata.vendas.repository;
 
 import br.com.alterdata.vendas.model.Categoria;
-import br.com.alterdata.vendas.model.Produto;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.List;
-
 @Repository
-public interface ProdutoRepository extends JpaRepository<Produto, Long> {
-    List<Produto> findByCategoria(Categoria categoria);
+public interface CategoriaRepository extends JpaRepository<Categoria, Long> {
+    Categoria findByNome(String nome);
 }
